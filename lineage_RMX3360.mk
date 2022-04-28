71edaea675cac4cbe8fb9d32c695936b02cb6c74
pjgowtham <pjgowtham@gmail.com>
2022-04-28 12:44:05 +0530

    RMX3360: Rebrand to GT Master Edition

--- lineage_RMX3360.mk
+++ lineage_RMX3360.mk
@@ -18,23 +18,21 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit from lemonade device
-$(call inherit-product, device/oneplus/lemonade/device.mk)
+# Inherit from RMX3360 device
+$(call inherit-product, device/oplus/RMX3360/device.mk)
 
 # Inherit some common Lineage stuff.
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
-PRODUCT_NAME := lineage_lemonade
-PRODUCT_DEVICE := lemonade
-PRODUCT_MANUFACTURER := OnePlus
-PRODUCT_BRAND := OnePlus
-PRODUCT_MODEL := LE2113
+PRODUCT_NAME := lineage_RMX3360
+PRODUCT_DEVICE := RMX3360
+PRODUCT_MANUFACTURER := realme
+PRODUCT_BRAND := realme
+PRODUCT_MODEL := RMX3360
 
-PRODUCT_GMS_CLIENTID_BASE := android-oneplus
+PRODUCT_SYSTEM_NAME := RMX3360
+PRODUCT_SYSTEM_DEVICE := RE54ABL1
 
-PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="OnePlus9_EEA-user 12 SKQ1.210216.001 R.202203102003 release-keys" \
-    TARGET_DEVICE=OnePlus9 \
-    TARGET_PRODUCT=OnePlus9_EEA
+PRODUCT_GMS_CLIENTID_BASE := android-oppo
 
-BUILD_FINGERPRINT := OnePlus/OnePlus9_EEA/OnePlus9:12/SKQ1.210216.001/R.202203102003:user/release-keys
+BUILD_FINGERPRINT := realme/RMX3360/RE54ABL1:12/RKQ1.210503.001/R.202202112205:user/release-keys
