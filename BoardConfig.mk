208d445a3a37133e176b9e9e18d152b3b6bacc4e
pjgowtham <pjgowtham@gmail.com>
2022-04-29 19:31:17 +0530

    RMX3360: Add logic for prebuilt kernel/DTB/DTBO

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -14,10 +14,21 @@
 # limitations under the License.
 #
 
-# Inherit from oplus sm8350-common
--include device/oplus/sm8350-common/BoardConfigCommon.mk
-
 DEVICE_PATH := device/oplus/RMX3360
+
+# kernel/DTB
+DEVICE_USES_PREBUILT_DTB := true
+PREBUILT_PATH := $(DEVICE_PATH)/prebuilt
+
+ifeq ($(DEVICE_USES_PREBUILT_DTB),true)
+BOARD_PREBUILT_DTBOIMAGE := $(PREBUILT_PATH)/dtbo.img
+TARGET_PREBUILT_DTB := $(PREBUILT_PATH)/dtb.img
+endif
+
+ifeq ($(TARGET_FORCE_PREBUILT_KERNEL),true)
+DEVICE_USES_PREBUILT_DTB := true
+TARGET_PREBUILT_KERNEL := $(PREBUILT_PATH)/Image.gz
+endif
 
 # Bluetooth
 BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := $(DEVICE_PATH)/bluetooth/include
@@ -30,6 +41,9 @@ TARGET_VENDOR_PROP += $(DEVICE_PATH)/vendor.prop
 
 # Recovery
 TARGET_RECOVERY_UI_MARGIN_HEIGHT := 100
+
+# Inherit from oplus sm8350-common
+-include device/oplus/sm8350-common/BoardConfigCommon.mk
 
 # inherit from the proprietary version
 -include vendor/oplus/RMX3360/BoardConfigVendor.mk
